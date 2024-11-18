8a6508d0d076188f2468d6116007ef62bcbf2c4a
Ashley Stewart <a.stewart.au@gmail.com>
2024-11-18 15:39:00 -0600

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dcm-check",
-    version="0.1.0",
+    version="0.1.1",
     description="A tool for checking DICOM compliance against a reference model using Pydantic",
     author="Ashley Stewart",
     url="https://github.com/astewartau/BrainBench",
