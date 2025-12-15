9d3d76e248df3022ec929f1eb58e40e548a74e33
Ashley Stewart <a.stewart.au@gmail.com>
2025-12-15 14:37:28 +1000

    v0.1.41

--- dicompare/__init__.py
+++ dicompare/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.40"
+__version__ = "0.1.41"
 
 # Import core functionalities
 from .io import get_dicom_values, load_dicom, load_schema, validate_schema, load_dicom_session, async_load_dicom_session, load_nifti_session, load_pro_file, load_pro_session, generate_test_dicoms_from_schema, generate_test_dicoms_from_schema_json, load_pro_file_schema_format
